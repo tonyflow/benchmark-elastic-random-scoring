27b68bc8227964f0190c05c0a1bef40f55990d22
Nikos Strongioglou <nikos.strongioglou@upstreamsystems.com>
2015-11-19 15:35:11 +0200

    test implementation

--- src/main/java/site/test/ElasticTest.java
+++ src/main/java/site/test/ElasticTest.java
@@ -1,23 +1,32 @@
 package site.test;
 
+import java.util.UUID;
+
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
-import org.elasticsearch.index.query.MatchAllQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
-import org.elasticsearch.index.query.TermFilterBuilder;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.sort.SortBuilders;
 
 public class ElasticTest {
 
 	@SuppressWarnings("resource")
 	public static void main(String[] args) {
 		InetSocketTransportAddress localInetSocketTransportAddress = new InetSocketTransportAddress("localhost", 9300);
-		InetSocketTransportAddress remoteInetSocketTransportAddress = new InetSocketTransportAddress("lhvmsrv81.lh.upstreamsystems.com", 9300);
 
-		TransportClient client = new TransportClient().addTransportAddresses(remoteInetSocketTransportAddress, localInetSocketTransportAddress);
-		SearchResponse result = client.prepareSearch("mtel_user_idx_1_3").setTypes("user")
-				.setQuery(QueryBuilders.filteredQuery(new MatchAllQueryBuilder(), new TermFilterBuilder("lala", true)))
+		Integer samplingSize = 30;
+		TransportClient client = new TransportClient().addTransportAddresses(localInetSocketTransportAddress);
+		SearchResponse result = client.prepareSearch("big_accounts").setTypes("user")
+				.setQuery(QueryBuilders.rangeQuery("balance").gte(1500))
+				.setSize(samplingSize)
+				.addSort(SortBuilders.scriptSort("sampling", "number").lang("groovy").param("salt", UUID.randomUUID().toString().substring(0, 9)))
 				.execute().actionGet();
+
+		System.out.println("Total results fetched :: " + result.getHits().getHits().length);
+		for (SearchHit h : result.getHits()) {
+			System.out.println("Balance is :: " + h.getSource().get("balance"));
+		}
 
 	}
 
